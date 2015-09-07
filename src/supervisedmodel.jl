5d62f39209118ca7c4aa3ff2f29f0dfcca8c1c75
Christof Stocker <christof.stocker@aimc.at>
2015-09-07 18:23:43 +0200

    add skeleton for SupervisedModel and delegating functions

--- src/supervisedmodel.jl
+++ src/supervisedmodel.jl
@@ -25,3 +25,45 @@ train!(callback::Function, model::Any, data::DataSource, solver::AbstractSolver,
 
 train!(model::Any, data::DataSource, solver::AbstractSolver, args...; nargs...) =
   train!(defaultCallback, model, data, solver, args...; nargs...)
+
+# ==========================================================================
+# Embedding supervised learning model for convenience
+
+type SupervisedModel{T<:Any}
+  _native::T
+  _state::Symbol
+  _history::TrainingHistory{Int}
+end
+
+# ==========================================================================
+# Macros for simpler function defintion
+
+macro _static_always(f, command, args...)
+  local vars = map(x->x.args[1], args)
+  esc(:(($f{T<:Any})(model::SupervisedModel{T},$(args...)) = $command))
+end
+
+macro _delegate_trained_only(f, args...)
+  local vars = map(x->x.args[1], args)
+  esc(:(($f{T<:Any})(model::SupervisedModel{T},$(args...)) = model._state == :untrained ? throw(ArgumentError("Function not defined for untrained models")) : $f(model._native,$(vars...))))
+end
+
+macro _delegate_trained_else(f, alternative, args...)
+  local vars = map(x->x.args[1], args)
+  esc(:(($f{T<:Any})(model::SupervisedModel{T},$(args...)) = model._state == :untrained ? $alternative : $f(model._native,$(vars...))))
+end
+
+# ==========================================================================
+# Define the local and delegating functions
+
+@_static_always(state, model._state)
+
+@_delegate_trained_else(iterations, 0)
+
+@_delegate_trained_only(cost)
+@_delegate_trained_only(cost, data::DataSource)
+@_delegate_trained_only(fitness)
+@_delegate_trained_only(fitness, data::DataSource)
+@_delegate_trained_only(predict, data::DataSource)
+@_delegate_trained_only(predictProb, data::DataSource)
+@_delegate_trained_only(accuracy, data::DataSource)
